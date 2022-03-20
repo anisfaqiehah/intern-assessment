1d191e6eab5176a4d857f6ac8151fd3a52553b0b
anisfaqiehah <78006058+anisfaqiehah@users.noreply.github.com>
2022-03-20 12:37:44 +0000

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,16 @@
+print('Enter row number: ')
+rowNum = input()
+print('Enter column number: ')
+columnNum = input()
+
+rowNB = [-1, -1, -1, 0, 0, 1, 1, 1]
+colNB = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+region = []
+def checked(row,colm):
+    global rowNum, columnNum
+
+def dfs(visited, matrix, cell):
+    if cell not in visited:
+        print (cell)
+        region.append(cell)
